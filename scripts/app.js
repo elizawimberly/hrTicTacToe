d4f5198059612fa8ae385507e1e50727159a084f
Eliza <elizawimberly@gmail.com>
2021-12-13 21:18:18 -0800

    able to select x or o and place that value in the matrix at the corresponding position

--- scripts/app.js
+++ scripts/app.js
@@ -4,19 +4,75 @@ myHeading.textContent = 'TIC TAC TOE';
 document.querySelectorAll('td')
 .forEach(element => element.addEventListener("click", function(event) {
     // Here, `this` refers to the element the event was hooked on
+    //click to get event.target.id
+    //use that val to look up matrix position
+    //add select input to matrix position -- x or o
     console.log("clicked", event.target.id)
-}
-
+  }
 ));
 
-function reply_click(){
-    console.log(event.srcElement.id);
+document.querySelectorAll('select')
+.forEach(element => element.addEventListener('change', (event) => {
+    var selected = event.target.value; //THIS GET THE X OR O!
+    console.log('selected:', selected)
+    var location = element.closest('td').id.toString();
+    console.log(location)
+    var position = tableMap[location];
+    console.log(position)
+    matrix[position[0]][position[1]] = selected;
+  }));
+
+
+// selectElement.addEventListener('change', (event) => {
+//   const result = document.querySelector('.result');
+//   result.textContent = `You like ${event.target.value}`;
+// });
+
+// function update() {
+//   var select = document.getElementById('1A');
+//   var option = select.options[select.selectedIndex];
+//   console.log(option)
+// }
+
+var row1 = new Array(3);
+var row2 = new Array(3);
+var row3 = new Array(3);
+
+var matrix = [
+  row1,//0
+  row2,//1
+  row3//2
+]
+
+var tableMap = {
+  '1A': [0, 0],
+  '1B': [0, 1],
+  '1C': [0, 2],
+  '2A': [1, 0],
+  '2B': [1, 1],
+  '2C': [1, 2],
+  '3A': [2, 0],
+  '3B': [2, 1],
+  '3C': [2, 2]
 }
 
-// var board = {
-//   '1A': undefined,
-//   '1B':
-// }
+/*
+1A = matrix[0][0]
+1B = matrix[0][1]
+1C = matrix[0][2]
+2A = matrix[1][0]
+2B = matrix[1][1]
+2C = matrix[1][2]
+3A = matrix[2][0]
+3B = matrix[2][1]
+3C = matrix[2][2]
+*/
+
+var dogs = {
+  soren: 'blonde'
+}
+
+
 
 //could collect in matrix
 /*
